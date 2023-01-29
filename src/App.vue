aede65ee665dd38d5b89793bc5d64c12e355c276
lambert18 <lambertpaulinus@gmail.com>
2023-01-29 23:14:42 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <ProductDisplay></ProductDisplay>
+</template>
+
+<script>
+
+import ProductDisplay from './components/ProductDisplay.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ProductDisplay
+  },
+  data(){
+		return {
+			show: true
+		}
+	},
+    mounted(){
+        setTimeout(() => {
+            this.show = false
+        }, 2500)
+    }
+};
+
+</script>
+
+<style>
+
+
+
+</style>
